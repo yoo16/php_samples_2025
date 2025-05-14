6cf8f04a665fcc72710d1ca271bf43ba76c6e2c2
yoo16 <yohei.yoshikawa@gmail.com>
2025-05-15 02:21:42 +0900

    update

--- 11_layout_view/app/views/layouts/app.layout.php
+++ 11_layout_view/app/views/layouts/app.layout.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html lang="ja">
 
-<?php include COMPONENT_DIR . 'head.php' ?>
+<?php include COMPONENT_DIR . 'Head.php' ?>
 
 <body>
-    <?php include COMPONENT_DIR . 'loading.php'; ?>
+    <?php include COMPONENT_DIR . 'Loading.php'; ?>
 
-    <?php include COMPONENT_DIR . 'nav.php' ?>
+    <?php include COMPONENT_DIR . 'Nav.php' ?>
 
     <main class="container mx-auto py-8">
         <?php include $view_path ?>
     </main>
 
-    <?php include COMPONENT_DIR . 'footer.php' ?>
+    <?php include COMPONENT_DIR . 'Footer.php' ?>
 </body>
 
 </html>
